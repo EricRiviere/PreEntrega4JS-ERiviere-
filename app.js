189c59a0153d2dd0008df5a33e71727067177709
Eric Riviere <eric.riviere.ponsa@gmail.com>
2023-06-18 08:00:58 +0200

    initial version

---
+++ app.js
@@ -0,0 +1,526 @@
+//Product Class
+class Product {
+  //Product constructor
+  constructor(productName, productPrice, productColor, productImage, amount) {
+    this.productName = productName;
+    this.productPrice = productPrice;
+    this.productColor = productColor;
+    this.productImage = productImage;
+    this.amount = amount;
+  }
+}
+
+//Products creation
+const evolveWhite = new Product(
+  "Evolve 3D Pro Air Jersey",
+  225,
+  "White",
+  "images/img-man/maillot-man1.webp",
+  1
+);
+const evolveGray = new Product(
+  "Evolve 3D Pro Air Jersey",
+  225,
+  "Gray",
+  "images/img-man/maillot-man2.webp",
+  1
+);
+const evolveGreen = new Product(
+  "Evolve 3D Pro Air Jersey",
+  225,
+  "Green",
+  "images/img-man/maillot-man3.webp",
+  1
+);
+const evadeBlack = new Product(
+  "Evade Pro Base Jersey 2.0",
+  245,
+  "Black",
+  "images/img-man/maillot-man4.webp",
+  1
+);
+const evadeGray = new Product(
+  "Evade Pro Base Jersey 2.0",
+  245,
+  "Gray",
+  "images/img-man/maillot-man5.webp",
+  1
+);
+const evadeGreen = new Product(
+  "Evade Pro Base Jersey 2.0",
+  245,
+  "Green",
+  "images/img-man/maillot-man6.webp",
+  1
+);
+const trainingBibBlack = new Product(
+  "Training Bib 3.0",
+  295,
+  "Black",
+  "images/img-man/culotte-man1.webp",
+  1
+);
+const elipseBibBlack = new Product(
+  "Elipse Team Bib Evo",
+  375,
+  "Black",
+  "images/img-man/culotte-man2.webp",
+  1
+);
+const trainingBibBrown = new Product(
+  "Training Bib 3.0",
+  295,
+  "Brown",
+  "images/img-man/culotte-man3.webp",
+  1
+);
+const trainingBibGreen = new Product(
+  "Training Bib 3.0",
+  295,
+  "Green",
+  "images/img-man/culotte-man4.webp",
+  1
+);
+const elipseBibRed = new Product(
+  "Elipse Team Bib Evo",
+  375,
+  "Red",
+  "images/img-man/culotte-man5.webp",
+  1
+);
+const teamBibBrown = new Product(
+  "Team Bib Evo",
+  355,
+  "Brown",
+  "images/img-man/culotte-man6.webp",
+  1
+);
+const evadeLsBrown = new Product(
+  "Evade Pro Base LS Jersey 2.0",
+  275,
+  "Brown",
+  "images/img-NS/NS3.webp",
+  1
+);
+const evadeLsPurple = new Product(
+  "Evade Pro Base LS Jersey 2.0",
+  275,
+  "Purple",
+  "images/img-NS/NS4.webp",
+  1
+);
+
+const wEvadePurple = new Product(
+  "Women's Evade Pro Base Jersey 2.0",
+  245,
+  "Purple",
+  "images/img-NS/NS6.webp",
+  1
+);
+
+const wEvadeBlue = new Product(
+  "Women's Evade Pro Base Jersey 2.0",
+  245,
+  "Blue",
+  "images/img-NS/NS7.webp",
+  1
+);
+const evadeWhite = new Product(
+  "Evade Pro Base Jersey 2.0",
+  245,
+  "White",
+  "images/img-NS/NS8.webp",
+  1
+);
+
+const wTeamBibEvoPurple = new Product(
+  "Women's Team Bib Evo",
+  355,
+  "Purple",
+  "images/img-NS/NS9.webp",
+  1
+);
+
+const wTeamBibEvoBrown = new Product(
+  "Women's Team Bib Evo",
+  355,
+  "Brown",
+  "images/img-NS/NS10.webp",
+  1
+);
+
+const teamBibEvoDeepPurple = new Product(
+  "Team Bib Evo",
+  355,
+  "Deep Purple",
+  "images/img-NS/NS11.webp",
+  1
+);
+
+const teamBibEvoPurple = new Product(
+  "Women's Team Bib Evo",
+  355,
+  "Purple",
+  "images/img-NS/NS9.webp",
+  1
+);
+
+const draftTeamJacketBrown = new Product(
+  "Draft Team Jacket",
+  215,
+  "Brown",
+  "images/img-NS/NS15.webp",
+  1
+);
+
+const wDraftTeamJacketPurple = new Product(
+  "Women's Draft Team Jacket",
+  215,
+  "Purple",
+  "images/img-NS/NS16.webp",
+  1
+);
+
+const wEvolveWhite = new Product(
+  "Women's Evolve 3D Pro Air",
+  225,
+  "White",
+  "images/img-woman/maillot-woman1.webp",
+  1
+);
+
+const wEvolveGreen = new Product(
+  "Women's Evolve 3D Pro Air",
+  225,
+  "Green",
+  "images/img-woman/maillot-woman2.webp",
+  1
+);
+
+const wEvadeLightGreen = new Product(
+  "Women's Evade Pro Base Jersey 2.0",
+  245,
+  "Light Green",
+  "images/img-woman/maillot-woman4.webp",
+  1
+);
+
+const wEvolvePink = new Product(
+  "Women's Evolve 3D Pro Air",
+  225,
+  "Pink",
+  "images/img-woman/maillot-woman5.webp",
+  1
+);
+
+const teamBibEvoLightGray = new Product(
+  "Team Bib Evo",
+  355,
+  "Light Gray",
+  "images/img-woman/culotte-woman1.webp",
+  1
+);
+
+const wtrainingBibBlack = new Product(
+  "Women's Training Bib 3.0",
+  295,
+  "Black",
+  "images/img-woman/culotte-woman2.webp",
+  1
+);
+
+const wtrainingBibGreen = new Product(
+  "Women's Training Bib 3.0",
+  295,
+  "Green",
+  "images/img-woman/culotte-woman3.webp",
+  1
+);
+
+const wAltRoadCargoPurple = new Product(
+  "Women's Alt_Road Cargo",
+  415,
+  "Purple",
+  "images/img-woman/culotte-woman4.webp",
+  1
+);
+
+const wAltRoadCargoBrown = new Product(
+  "Women's Alt_Road Cargo",
+  415,
+  "Brown",
+  "images/img-woman/culotte-woman5.webp",
+  1
+);
+
+const wtrainingBibBrown = new Product(
+  "Women's Training Bib 3.0",
+  295,
+  "Brown",
+  "images/img-woman/culotte-woman6.webp",
+  1
+);
+
+const maapX100Red = new Product(
+  "MAAP x 100% Hypercraft",
+  305,
+  "Red",
+  "images/img-acc/acc1.webp",
+  1
+);
+
+const maapX100Green = new Product(
+  "MAAP x 100% Hypercraft",
+  305,
+  "Green",
+  "images/img-acc/acc2.webp",
+  1
+);
+
+const proRaceMittGreen = new Product(
+  "Pro Race Mitt",
+  115,
+  "Green",
+  "images/img-acc/acc3.webp",
+  1
+);
+
+const legWarmersBlack = new Product(
+  "Leg Warmers",
+  95,
+  "Black",
+  "images/img-acc/acc4.webp",
+  1
+);
+
+const legWarmersBlue = new Product(
+  "Leg Warmers",
+  95,
+  "Blue",
+  "images/img-acc/acc5.webp",
+  1
+);
+
+const kneeWarmersBlack = new Product(
+  "Knee Warmers",
+  80,
+  "Black",
+  "images/img-acc/acc6.webp",
+  1
+);
+
+const armWarmersBlue = new Product(
+  "Arm Warmers",
+  75,
+  "Blue",
+  "images/img-acc/acc7.webp",
+  1
+);
+
+const evadeBottleBlack = new Product(
+  "Evade Bottle",
+  30,
+  "Black",
+  "images/img-acc/acc8.webp",
+  1
+);
+
+const evadeBottleWhite = new Product(
+  "Evade Bottle",
+  30,
+  "White",
+  "images/img-acc/acc9.webp",
+  1
+);
+
+const ADL2023Socks = new Product(
+  "ADL 2023 Sock",
+  35,
+  "Black",
+  "images/img-acc/acc10.webp",
+  1
+);
+
+const emergeProAirSockGreen = new Product(
+  "Emerge Pro Air Sock",
+  30,
+  "Green",
+  "images/img-acc/acc11.webp",
+  1
+);
+
+const emergeProAirSockBlack = new Product(
+  "Emerge Pro Air Sock",
+  30,
+  "Black",
+  "images/img-acc/acc12.webp",
+  1
+);
+
+//Gloval Variables
+const cart = [];
+
+//Elements
+let subtotal = 0;
+let total = 0;
+const totalElement = document.querySelector("#total");
+totalElement.innerText = total;
+const cartElement = document.querySelector(".cart");
+const numProdInCartElement = document.querySelector(".numberOfProductsInCart");
+
+//Functions
+function addProduct(product) {
+  let existingProduct = onCart(product);
+  if (existingProduct) {
+    existingProduct.amount += 1;
+  } else {
+    cart.push(product);
+  }
+  subtotal = calculateSubtotal();
+  total = calculateTotal();
+  renderCart();
+  renderCartIcon();
+}
+
+function plusProductsCart(index) {
+  const product = cart[index];
+  if (product.amount >= 1) {
+    product.amount += 1;
+    subtotal = calculateSubtotal();
+    total = calculateTotal();
+    renderCart();
+    renderCartIcon();
+  }
+}
+
+function removeProduct(index) {
+  const product = cart[index];
+  if (product.amount > 1) {
+    product.amount -= 1;
+  } else {
+    cart.splice(index, 1);
+  }
+  subtotal = calculateSubtotal();
+  total = calculateTotal();
+  renderCart();
+  renderCartIcon();
+}
+
+function onCart(product) {
+  return cart.find(
+    (existingProduct) =>
+      existingProduct.productName == product.productName &&
+      existingProduct.productColor == product.productColor
+  );
+}
+
+function renderCart() {
+  cartElement.innerHTML = "";
+  for (const product of cart) {
+    let productIndex = cart.indexOf(product);
+    let productElement = `
+<li class="list-group-item">
+  <div class="row text-center">
+    <div class"col">
+      <div class="mt-2">PRODUCT:</div>
+      <div class="mt-2">${product.productName}</div>
+      <div class="mt-2">Price: ${product.productPrice} AUD $</div>
+      <div class="mt-2">Color: ${product.productColor}</div>
+      <a class="mt-1 mx-1 btn btn-sm btn-outline-secondary border-1 py-1" href="#" onclick="removeProduct(${productIndex})"><i class="bi bi-dash-lg"></i></a><a class="mt-1 mx-1 btn border-1 py-1" href="#">${product.amount}</a><a class="mt-1 mx-1 btn btn-sm btn-outline-secondary border-1 py-1" onclick="plusProductsCart(${productIndex})" href="#"><i class="bi bi-plus-lg"></i></a>
+    </div>
+    <div class="col">
+      <img src="../${product.productImage}" class"img-fluid card-img-top">
+    </div>
+  </div>
+</li>
+`;
+    cartElement.innerHTML += productElement;
+  }
+  totalElement.innerText = total;
+}
+
+function renderCartIcon() {
+  numProdInCartElement.innerText = cart.length;
+  numProdInCartElement.classList.add(
+    "position-absolute",
+    "top-0",
+    "start-100",
+    "translate-middle",
+    "badge",
+    "rounded-pill",
+    "bg-danger"
+  );
+}
+
+function calculateSubtotal() {
+  let sub = 0;
+  for (const product of cart) {
+    sub += product.productPrice * product.amount;
+  }
+  return sub;
+}
+
+function calculateTotal() {
+  let tot = 0;
+  for (const product of cart) {
+    tot += product.productPrice * product.amount;
+  }
+  return tot;
+}
+
+function emptyCart() {
+  while (cart.length > 0) {
+    cart.pop();
+  }
+  cartElement.innerHTML = "";
+  renderCartIcon();
+  total = 0;
+  totalElement.innerText = total;
+}
+
+function printTicket() {
+  const currentDate = new Date().toLocaleDateString();
+
+  const ticketContainer = document.createElement("div");
+  ticketContainer.classList.add("ticket");
+
+  const ticketHeader = document.createElement("h2");
+  ticketHeader.classList.add("text-center");
+  ticketHeader.innerText = "TICKET";
+
+  const productList = document.createElement("ul");
+  productList.classList.add("list-unstyled", "list-group");
+
+  for (const product of cart) {
+    const productItem = document.createElement("li");
+    productItem.innerHTML = `
+      <div class="list-group-item">
+        <div><strong>Product:</strong> ${product.productName}</div>
+        <div><strong>Price:</strong> ${product.productPrice} AUD $</div>
+        <div><strong>Color:</strong> ${product.productColor}</div>
+        <div><strong>Amount:</strong> ${product.amount}</div>
+      </div>
+    `;
+    productList.appendChild(productItem);
+  }
+
+  const ticketTotalElement = document.createElement("div");
+  ticketTotalElement.innerHTML = `<strong>Total:</strong> ${total} AUD $`;
+
+  const dateElement = document.createElement("div");
+  dateElement.innerHTML = `<strong>Date:</strong> ${currentDate}`;
+
+  ticketContainer.appendChild(ticketHeader);
+  ticketContainer.appendChild(productList);
+  ticketContainer.appendChild(ticketTotalElement);
+  ticketContainer.appendChild(dateElement);
+
+  const ticketContainerDiv = document.querySelector(".cart");
+  ticketContainerDiv.innerHTML = "";
+  ticketContainerDiv.appendChild(ticketContainer);
+
+  const totalElement = document.querySelector(".total");
+  totalElement.innerHTML = "";
+  const checkoutElement = document.querySelector(".checkout");
+  checkoutElement.innerHTML = "";
+}
