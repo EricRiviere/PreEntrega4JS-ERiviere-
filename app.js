c4dfec37661b60f43307678314d890309bf8ef97
Eric Riviere <eric.riviere.ponsa@gmail.com>
2023-07-13 19:50:36 +0200

    muted :(

--- app.js
+++ app.js
@@ -553,7 +553,7 @@ class Cart {
       const modalIframe = document.querySelector("#modalIframe");
       modalIframe.classList.add("modal-iframe");
       modalIframe.innerHTML = `
-      <video width="400" autoplay class="responsive-iframe">
+      <video width="400" autoplay muted class="responsive-iframe">
         <source src="/media/video.mp4" type="video/mp4">
         <source src="/media/video.ogg" type="video/ogg">
       </video>
